8706a053a71973fdbf649d6c10801d50bbefc133
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-04-05 12:41:42 -0300

    Removing large files

--- analise-de-dados-e-boas-praticas/mvp/dados/dados.sh
+++ analise-de-dados-e-boas-praticas/mvp/dados/dados.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-#
-# Busca dados originais
-#
 wget -i dados.txt
 
-#
-# Transforma *.csv.gz => *.csv.zip
-#
 for f in $(ls *.gz);
 do
 	gunzip "$f"
 	rm -f "$f"
-	f_no_extension="${f%.*}"
-	zip "$f_no_extension".zip "$f_no_extension"
-	rm -f "$f_no_extension"
 done
