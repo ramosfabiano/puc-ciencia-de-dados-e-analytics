914fe54ad9ee2d4a904971e9750555a8e5de7989
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-09-19 10:13:44 -0300

    Atualizando imagens

--- engenharia-de-dados/mvp/scripts/data-collect.py
+++ engenharia-de-dados/mvp/scripts/data-collect.py
@@ -5,4 +5,4 @@ s3_client = boto3.client('s3', region_name='us-east-1')
 
 req = requests.get(url=f'https://raw.githubusercontent.com/pragmaerror/puc-ciencia-de-dados-e-analytics/main/engenharia-de-dados/mvp/data/Airline_Dataset_Updated.csv')
 csv_content = req.text
-s3_client.put_object(Body=csv_content, Bucket='aws-glue-rawdata', Key=f'Airline_Dataset_Updated.csv') 
+s3_client.put_object(Body=csv_content, Bucket='aws-glue-rawdata', Key=f'airline-tbl/Airline_Dataset_Updated.csv') 
