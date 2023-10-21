018e9a245bc87b8fff013a81cfed047627a86106
Fabiano Ramos <ramos_fabiano@yahoo.com.br>
2023-10-20 21:48:33 -0300

    Ajustes após renomeação de usuário

--- engenharia-de-dados/mvp/scripts/data-collect.py
+++ engenharia-de-dados/mvp/scripts/data-collect.py
@@ -3,6 +3,6 @@ import boto3
 
 s3_client = boto3.client('s3', region_name='us-east-1')
 
-req = requests.get(url=f'https://raw.githubusercontent.com/pragmaerror/puc-ciencia-de-dados-e-analytics/main/engenharia-de-dados/mvp/data/Airline_Dataset_Updated.csv')
+req = requests.get(url=f'https://raw.githubusercontent.com/ramosfabiano/puc-ciencia-de-dados-e-analytics/main/engenharia-de-dados/mvp/data/Airline_Dataset_Updated.csv')
 csv_content = req.text
 s3_client.put_object(Body=csv_content, Bucket='aws-glue-rawdata', Key=f'airline-tbl/Airline_Dataset_Updated.csv') 
